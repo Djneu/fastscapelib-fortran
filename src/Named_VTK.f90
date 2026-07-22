d733a860f0d3238a6a61c08941139aceffede64c
djneu <derekneuharth@gmail.com>
2026-07-22 11:10:25 +0200

    Initial commit with change to vts

--- src/Named_VTK.f90
+++ src/Named_VTK.f90
@@ -1,96 +1,241 @@
-subroutine Fastscape_Named_VTK (f, vex, istep, foldername, k)
+subroutine Fastscape_Named_VTK (f, vex, istep, foldername, k, model_height, model_dim, adjustment)
 
-    ! Subroutine modified from VTK.f90 to create a simple
-    ! VTK file inside of a specified folder path.
-    ! This additionally outputs the topography, basement
-    ! erosion rate, total erosion, drainage area, and
-    ! catchment on top of the given HHHHH field.
-    ! This assumes the output folder is created beforehand.
+  ! Writes an XML StructuredGrid (.vts) file per timestep into
+  ! <foldername>/, preserving the regular nx*ny lattice (connectivity
+  ! is implicit, so no Cells block is needed).
+  !
+  ! Companion routine Fastscape_PVD_Collection maintains a .pvd that
+  ! indexes each .vts with its physical time, giving ParaView a proper
+  ! time series that plots directly alongside ASPECT's solution.pvd.
+  !
+  ! This version writes ASCII (inline) data: simplest to get correct and
+  ! trivially inspectable. Move to appended-binary later if you want to
+  ! match the file size / speed of the original binary .vtk output.
 
-    use FastScapeContext
-    implicit none
+  use FastScapeContext
+  implicit none
 
-    integer, intent(in) :: k, istep
-    double precision, intent(in) :: vex
-    double precision, intent(in), dimension(*) :: f
-    character(len=k), intent(in) :: foldername
-    character cstep*7
+  integer, intent(in) :: k, istep, model_dim
+  double precision, intent(in) :: vex, model_height, adjustment
+  double precision, intent(in), dimension(*) :: f
+  character(len=k), intent(in) :: foldername
+  character cstep*7
 
-    integer nheader,nfooter,npart1,npart2, ftime
-    character header*1024,footer*1024,part1*1024,part2*1024,nxc*6,nyc*6,nnc*12
-    integer i,j
-    double precision dx,dy
+  integer :: i, j
+  double precision :: dx, dy
+  character(len=1024) :: fname
+  character(len=64)   :: extent
 
-    dx = xl/(nx - 1)
-    dy = yl/(ny - 1)
+  dx = xl/(nx - 1)
+  dy = yl/(ny - 1)
 
-    write (cstep,'(i7)') istep
-    if (istep.lt.10) cstep(1:6)='000000'
-    if (istep.lt.100) cstep(1:5)='00000'
-    if (istep.lt.1000) cstep(1:4)='0000'
-    if (istep.lt.10000) cstep(1:3)='000'
-    if (istep.lt.100000) cstep(1:2)='00'
-    if (istep.lt.1000000) cstep(1:1)='0'
+  ! zero-padded step string (matches your original convention)
+  write (cstep,'(i7)') istep
+  if (istep.lt.10)      cstep(1:6)='000000'
+  if (istep.lt.100)     cstep(1:5)='00000'
+  if (istep.lt.1000)    cstep(1:4)='0000'
+  if (istep.lt.10000)   cstep(1:3)='000'
+  if (istep.lt.100000)  cstep(1:2)='00'
+  if (istep.lt.1000000) cstep(1:1)='0'
 
-    write (nxc,'(i6)') nx
-    write (nyc,'(i6)') ny
-    write (nnc,'(i12)') nn
+  ! StructuredGrid extent: 0-based index range in i, j, k
+  write(extent,'(I0,1x,I0,1x,I0,1x,I0,1x,I0,1x,I0)') 0, nx-1, 0, ny-1, 0, 0
 
-    header(1:1024)=''
-    header='# vtk DataFile Version 3.0'//char(10)//'FastScape'//char(10) &
-    //'BINARY'//char(10)//'DATASET STRUCTURED_GRID'//char(10) &
-    //'DIMENSIONS '//nxc//' '//nyc//' 1'//char(10)//'POINTS' &
-    //nnc//' float'//char(10)
-    nheader=len_trim(header)
-    footer(1:1024)=''
-    footer='POINT_DATA'//nnc//char(10)
-    nfooter=len_trim(footer)
-    part1(1:1024)=''
-    part1='SCALARS '
-    npart1=len_trim(part1)+1
-    part2(1:1024)=''
-    part2=' float 1'//char(10)//'LOOKUP_TABLE default'//char(10)
-    npart2=len_trim(part2)
+  fname = trim(foldername)//'/Topography'//cstep//'.vts'
+  open(unit=77, file=trim(fname), status='unknown', form='formatted')
 
-    ! Formatting to output the correct size for each visualization field.
-    open(unit=77,file=(foldername//'/Topography'//cstep//'.vtk'),status='unknown', &
-    form='unformatted',access='direct', recl=nheader+3*4*nn+nfooter+(npart1+10+npart2+4*nn) &
-    +(npart1+5+npart2+4*nn)+(npart1+8+npart2+4*nn)+(npart1+12+npart2+4*nn)+(npart1+13+npart2+4*nn) &
-    +(npart1+13+npart2+4*nn)+(npart1+9+npart2+4*nn))
-    write (77,rec=1) &
-    header(1:nheader), &
-    ((sngl(dx*(i-1)),sngl(dy*(j-1)),sngl(h(i+(j-1)*nx)*abs(vex)),i=1,nx),j=1,ny), &
-    footer(1:nfooter), &
-    part1(1:npart1)//'topography'//part2(1:npart2),sngl(h(1:nn)), &
-    part1(1:npart1)//'HHHHH'//part2(1:npart2),sngl(f(1:nn)), &
-    part1(1:npart1)//'basement'//part2(1:npart2),sngl(b(1:nn)), &
-    part1(1:npart1)//'erosion_rate'//part2(1:npart2),sngl(erate(1:nn)), &
-    part1(1:npart1)//'total_erosion'//part2(1:npart2),sngl(etot(1:nn)), &
-    part1(1:npart1)//'drainage_area'//part2(1:npart2),sngl(a(1:nn)) , &
-    part1(1:npart1)//'catchment'//part2(1:npart2),sngl(catch(1:nn))
-    close(77)
+  write(77,'(A)') '<?xml version="1.0"?>'
+  write(77,'(A)') '<VTKFile type="StructuredGrid" version="0.1" byte_order="LittleEndian">'
+  write(77,'(A)') '  <StructuredGrid WholeExtent="'//trim(extent)//'">'
+  write(77,'(A)') '    <Piece Extent="'//trim(extent)//'">'
 
-    if (vex.lt.0.d0) then
-      open(unit=77,file=(foldername//'/Basement'//cstep//'.vtk'),status='unknown',form='unformatted',access='direct', &
-      recl=nheader+3*4*nn+nfooter+(npart1+1+npart2+4*nn) &
-      +(npart1+5+npart2+4*nn))
-      write (77,rec=1) &
-      header(1:nheader), &
-      ((sngl(dx*(i-1)),sngl(dy*(j-1)),sngl(b(i+(j-1)*nx)*abs(vex)),i=1,nx),j=1,ny), &
-      footer(1:nfooter), &
-      part1(1:npart1)//'B'//part2(1:npart2),sngl(b(1:nn)), &
-      part1(1:npart1)//'HHHHH'//part2(1:npart2),sngl(f(1:nn))
-      close(77)
-      open(unit=77,file=(foldername//'/SeaLevel'//cstep//'.vtk'),status='unknown',form='unformatted',access='direct', &
-      recl=nheader+3*4*nn+nfooter+(npart1+2+npart2+4*nn))
-      write (77,rec=1) &
-      header(1:nheader), &
-      ((sngl(dx*(i-1)),sngl(dy*(j-1)),sngl(sealevel*abs(vex)),i=1,nx),j=1,ny), &
-      footer(1:nfooter), &
-      part1(1:npart1)//'SL'//part2(1:npart2),(sngl(sealevel),i=1,nn)
-      close(77)
-    endif
+  ! ---- Points (x, y, z = elevation * vertical exaggeration) ----
+  ! Order must be i fastest, then j, then k (VTK structured ordering).
+  write(77,'(A)') '      <Points>'
+  write(77,'(A)') '        <DataArray type="Float32" NumberOfComponents="3" format="ascii">'
+  do j = 1, ny
+     do i = 1, nx
+      if (model_dim == 2) then
+         ! 2D ASPECT: elevation in Y, Z = 0, adjust for ghost nodes and extent in 2D.
+         write(77,'(3(1x,ES14.6))') &
+              sngl(dx*(i-1)-adjustment), &
+              sngl((h(i+(j-1)*nx)+model_height)*abs(vex)), &
+              sngl(dy*(j-1)-yl-adjustment)
+              
+      else
+         ! 3D ASPECT: X, Y as-is, elevation in Z
+         write(77,'(3(1x,ES14.6))') &
+              sngl(dx*(i-1)-adjustment), &
+              sngl(dy*(j-1)-adjustment), &
+              sngl((h(i+(j-1)*nx))*abs(vex)+model_height)
+      end if
+     end do
+  end do
+  write(77,'(A)') '        </DataArray>'
+  write(77,'(A)') '      </Points>'
+
+  ! ---- Point data (all your original fields, same node ordering) ----
+  write(77,'(A)') '      <PointData Scalars="topography">'
+  call write_scalar(77, 'topography',    h,      nn)
+  call write_scalar(77, 'HHHHH',         f,      nn)
+  call write_scalar(77, 'basement',      b,      nn)
+  call write_scalar(77, 'erosion_rate',  erate,  nn)
+  call write_scalar(77, 'total_erosion', etot,   nn)
+  call write_scalar(77, 'drainage_area', a,      nn)
+  call write_scalar(77, 'catchment',     catch,  nn)
+  call write_scalar(77, 'precipitation',     precip,  nn)
+
+  ! Should these be renamed based on orientation, should z always read uplift?
+  call write_scalar(77, 'velocity_z',  u  ,  nn)
+  call write_scalar(77, 'velocity_x',  vx  ,  nn)
+  call write_scalar(77, 'velocity_y',  vy  ,  nn)
+  call write_scalar(77, 'diffusivity',  kd  ,  nn)
+  call write_scalar(77, 'river_incision_rate',  kf  ,  nn)
+  call write_scalar(77, 'velocity_y',  vy  ,  nn)
+  write(77,'(A)') '      </PointData>'
+
+  write(77,'(A)') '    </Piece>'
+  write(77,'(A)') '  </StructuredGrid>'
+  write(77,'(A)') '</VTKFile>'
+  close(77)
+
+  ! ---- Optional basement / sea-level files, mirroring the original ----
+  if (vex.lt.0.d0) then
+
+     fname = trim(foldername)//'/Basement'//cstep//'.vts'
+     open(unit=77, file=trim(fname), status='unknown', form='formatted')
+     write(77,'(A)') '<?xml version="1.0"?>'
+     write(77,'(A)') '<VTKFile type="StructuredGrid" version="0.1" byte_order="LittleEndian">'
+     write(77,'(A)') '  <StructuredGrid WholeExtent="'//trim(extent)//'">'
+     write(77,'(A)') '    <Piece Extent="'//trim(extent)//'">'
+     write(77,'(A)') '      <Points>'
+     write(77,'(A)') '        <DataArray type="Float32" NumberOfComponents="3" format="ascii">'
+     do j = 1, ny
+        do i = 1, nx
+          if (model_dim == 2) then
+            write(77,'(3(1x,ES14.6))') sngl(dx*(i-1)-adjustment), & 
+                  sngl(b(i+(j-1)*nx)*abs(vex)+model_height), &
+                  sngl(dy*(j-1)-yl-adjustment)
+          else
+            write(77,'(3(1x,ES14.6))') sngl(dx*(i-1)-adjustment), sngl(dy*(j-1)-adjustment), &
+                  sngl(b(i+(j-1)*nx)*abs(vex)+model_height)
+          endif
+        end do
+     end do
+     write(77,'(A)') '        </DataArray>'
+     write(77,'(A)') '      </Points>'
+     write(77,'(A)') '      <PointData Scalars="B">'
+     call write_scalar(77, 'B',     b, nn)
+     call write_scalar(77, 'HHHHH', f, nn)
+     write(77,'(A)') '      </PointData>'
+     write(77,'(A)') '    </Piece>'
+     write(77,'(A)') '  </StructuredGrid>'
+     write(77,'(A)') '</VTKFile>'
+     close(77)
+
+     fname = trim(foldername)//'/SeaLevel'//cstep//'.vts'
+     open(unit=77, file=trim(fname), status='unknown', form='formatted')
+     write(77,'(A)') '<?xml version="1.0"?>'
+     write(77,'(A)') '<VTKFile type="StructuredGrid" version="0.1" byte_order="LittleEndian">'
+     write(77,'(A)') '  <StructuredGrid WholeExtent="'//trim(extent)//'">'
+     write(77,'(A)') '    <Piece Extent="'//trim(extent)//'">'
+     write(77,'(A)') '      <Points>'
+     write(77,'(A)') '        <DataArray type="Float32" NumberOfComponents="3" format="ascii">'
+     do j = 1, ny
+        do i = 1, nx
+          if (model_dim == 2) then
+            write(77,'(3(1x,ES14.6))') sngl(dx*(i-1)-adjustment), sngl(sealevel*abs(vex)+model_height), &
+                  sngl(dy*(j-1)-yl-adjustment)
+          else
+            write(77,'(3(1x,ES14.6))') sngl(dx*(i-1)-adjustment), sngl(dy*(j-1)-adjustment), &
+                  sngl(sealevel*abs(vex)+model_height)
+          endif
+        end do
+     end do
+     write(77,'(A)') '        </DataArray>'
+     write(77,'(A)') '      </Points>'
+     write(77,'(A)') '      <PointData Scalars="SL">'
+     write(77,'(A)') '        <DataArray type="Float32" Name="SL" format="ascii">'
+     do i = 1, nn
+        write(77,'(1x,ES14.6)') sngl(sealevel)
+     end do
+     write(77,'(A)') '        </DataArray>'
+     write(77,'(A)') '      </PointData>'
+     write(77,'(A)') '    </Piece>'
+     write(77,'(A)') '  </StructuredGrid>'
+     write(77,'(A)') '</VTKFile>'
+     close(77)
+
+  end if
+
+  return
+
+contains
+
+  subroutine write_scalar(unit, name, arr, n)
+    integer, intent(in) :: unit, n
+    character(len=*), intent(in) :: name
+    double precision, intent(in), dimension(*) :: arr
+    integer :: ii
+    write(unit,'(A)') '        <DataArray type="Float32" Name="'//trim(name)// &
+         '" format="ascii">'
+    do ii = 1, n
+       write(unit,'(1x,ES14.6)') sngl(arr(ii))
+    end do
+    write(unit,'(A)') '        </DataArray>'
+  end subroutine write_scalar
+
+end subroutine Fastscape_Named_VTK
 
 
-    return
-  end subroutine Fastscape_Named_VTK
+subroutine Fastscape_PVD_Collection (istep, time, foldername, k)
+
+  ! Maintains <foldername>/Topography.pvd indexing each per-step .vts
+  ! with its physical time. Rewrites the whole (small) file each call so
+  ! the collection is always valid even if the run stops early.
+
+  implicit none
+  integer, intent(in) :: k, istep
+  double precision, intent(in) :: time
+  character(len=k), intent(in) :: foldername
+
+  integer, parameter :: MAXSTEPS = 100000
+  integer, save :: nrec = 0
+  integer, save :: steps(MAXSTEPS)
+  double precision, save :: times(MAXSTEPS)
+  character cstep*7
+  integer :: r
+  character(len=1024) :: fname
+
+  ! record this step
+  nrec = nrec + 1
+  if (nrec > MAXSTEPS) then
+     write(*,*) 'Fastscape_PVD_Collection: MAXSTEPS exceeded, increase it.'
+     nrec = MAXSTEPS
+     return
+  end if
+  steps(nrec) = istep
+  times(nrec) = time
+
+  fname = trim(foldername)//'/Topography.pvd'
+  open(unit=78, file=trim(fname), status='unknown', form='formatted')
+  write(78,'(A)') '<?xml version="1.0"?>'
+  write(78,'(A)') '<VTKFile type="Collection" version="0.1" byte_order="LittleEndian">'
+  write(78,'(A)') '  <Collection>'
+  do r = 1, nrec
+     write (cstep,'(i7)') steps(r)
+     if (steps(r).lt.10)      cstep(1:6)='000000'
+     if (steps(r).lt.100)     cstep(1:5)='00000'
+     if (steps(r).lt.1000)    cstep(1:4)='0000'
+     if (steps(r).lt.10000)   cstep(1:3)='000'
+     if (steps(r).lt.100000)  cstep(1:2)='00'
+     if (steps(r).lt.1000000) cstep(1:1)='0'
+     write(78,'(A,ES16.8,A)') '    <DataSet timestep="', times(r), &
+          '" group="" part="0" file="Topography'//cstep//'.vts"/>'
+  end do
+  write(78,'(A)') '  </Collection>'
+  write(78,'(A)') '</VTKFile>'
+  close(78)
+
+  return
+end subroutine Fastscape_PVD_Collection
